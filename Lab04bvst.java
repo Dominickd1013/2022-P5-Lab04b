290e9aa78df25e69bdcba870237d01043d2f7797
Dominick <domenico.damato@vtsd.com>
2022-10-13 11:20:33 -0400

    Updated as of 10/13/22

--- Lab04bvst.java
+++ Lab04bvst.java
@@ -14,28 +14,36 @@ public class Lab04bvst extends Applet
     {
         // DRAW CUBE
 
-        g.drawLine(300, 150, 550, 150); // background line top
-        g.drawLine(350, 250, 600, 250); // foreground line bottom top
-        g.drawLine(300, 150, 350, 250); // background left line top
-        g.drawLine(550, 150, 600, 250); // background line right top
+        int x175  = 175;
+        int y25  = 25;
+        int x425  = 425;
+        int xy225 = 225;
+        int y125  = 125;
+        int x475  = 475;
+        int y375  = 375;
 
 
-        g.drawLine(300, 350, 550, 350); // background line bottom
-        g.drawLine(350, 500, 600, 500); // foreground line bottom
-        g.drawLine(300, 350, 350, 500); // background left line
-        g.drawLine(550, 350, 600, 500); // background line right
+        g.drawLine(x175, y25, x425, y25); // background line top
+        g.drawLine(xy225, y125, x475, y125); // foreground line bottom top
+        g.drawLine(x175, y25, xy225, y125); // background left line top
+        g.drawLine(x425, y25, x475, y125); // background line right top
 
-        g.drawLine(300, 150, 300, 350); // background vertical line left
-        g.drawLine(550, 150, 550, 350); // background vertical line right
-        g.drawLine(350, 250, 350, 500); // foreground vertical line left
-        g.drawLine(600, 250, 600, 500); // foreground vertical line right
+
+        g.drawLine(x175, xy225, x425, xy225); // background line bottom
+        g.drawLine(xy225, y375, x475, y375); // foreground line bottom
+        g.drawLine(x175, xy225, xy225, y375); // background left line
+        g.drawLine(x425, xy225, x475, y375); // background line right
+
+        g.drawLine(x175, y25, x175, xy225); // background vertical line left
+        g.drawLine(x425, y25, x425, xy225); // background vertical line right
+        g.drawLine(xy225, y125, xy225, y375); // foreground vertical line left
+        g.drawLine(x475, y125, x475, y375); // foreground vertical line right
 
 
 
         // DRAW SPHERE
 
-        g.drawOval(750, 750, 500, 500,);
-
+        g.drawOval(750, 800, 800, 750);
 
 
 
